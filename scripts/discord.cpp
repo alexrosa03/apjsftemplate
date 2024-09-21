20910e8c0af11f1dce5c317cd36043f9af202e22
Alexis Dela Rosa <delarosauberstrike@gmail.com>
2024-09-21 16:33:14 +0800

    added discord rich presence functions

---
+++ scripts/discord.cpp
@@ -0,0 +1,11 @@
+class CfgDiscordRichPresence {
+    applicationID="1283437422725824604";
+    defaultDetails="PJSF";
+    defaultState="arma-ph.net";
+    defaultLargeImageKey="https://i.imgur.com/9dNpgNo.png";
+    defaultLargeImageText="PJSF";
+    defaultSmallImageKey="https://i.imgur.com/9dNpgNo.png";
+    defaultSmallImageText="Arma 3";
+    defaultButtons[]={"PJSF Discord","http://discord.gg/armaph","Website","https://arma-ph.net/"};
+    useTimeElapsed=1;
+};
